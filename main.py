88ea22e390dd14b6abda20673849c7467d933dc7
Matthias Lootens <matthiaslootens@hotmail.com>
2022-12-10 14:33:54 +0100

    Added dockerfile

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_restful import Api
 from REST.productionplan import Productionplan
+import os
 
 app = Flask(__name__)
 api = Api(app)
@@ -8,4 +9,5 @@ api = Api(app)
 api.add_resource(Productionplan, '/productionplan')
 
 if __name__ == "__main__":
-  app.run(port=8888)
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
